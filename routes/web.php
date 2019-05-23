3b05352229b5a1870cbd75cb3c9f3b09aeaa1901
Lukasz Gos <lukasz@nitro-digital.com>
2019-05-23 11:46:49 +0200

    Basics/Routing

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,50 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/cache', function () {
-    return Cache::get('key');
+Route::prefix('routeTests')->group(function () {
+
+
+    Route::get('/cache', function () {
+        return Cache::get('key');
+    });
+
+
+    Route::get('/foo', function () {
+        return 'FOO';
+    });
+
+    //multiple http methods
+    Route::match(['get', 'post'], '/test-match', function () {
+        return 'Test Match success';
+    });
+
+    //Route::get('form', function () {
+    //    return view('form');
+    //});
+    Route::view('/form', 'form');
+
+
+    Route::redirect('test-redirect', '/foo', 301);
+    //equivalent on Route::redirect('test-redirect', '/foo');
+
+
+    //Route parameters -----------------------------------------------------------------------------------------------------
+    Route::get('user/{id}', function ($id) {
+        return "User: $id";
+    });
+
+    Route::get('user-name/{name?}', function ($name = 'John') {
+        return $name;
+    })->where('name', '[A-Za-z]+');
+
+    Route::get('custom-id/{id}', function ($id) {
+        return $id;
+    });
+
+
+    //Named Route ----------------------------------------------------------------------------------------------------------
+    Route::get('/test-named-route', function () {
+        return 'named route';
+    })->name('namedRoute');
+
 });
